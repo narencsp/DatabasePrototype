12921d42b908e35eea5b94aacc5f51d2d23e15bd
Narendran <naren.csg@gmail.com>
2021-03-09 14:22:22 -0500

    pushing correct directory structure

---
+++ src/com/package/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import presentation.UserLogin;
+
+public class Main {
+    public static void main(String[] args) {
+        UserLogin ul = new UserLogin();
+        ul.getUsername();
+    }
+}
---
+++ src/com/package/presentation/UserLogin.java
@@ -0,0 +1,51 @@
+package presentation;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class UserLogin {
+    private String userName, password;
+
+
+    public void getUsername() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Username");
+        userName = in.nextLine();
+        System.out.println("Enter Password");
+        password = in.nextLine();
+
+        Boolean result = AuthenticateUser(userName, password);
+        if(result.equals(true))
+            System.out.println("User found in Table");
+        else
+            System.out.println("User not found in Table");
+    }
+
+    public Boolean AuthenticateUser(String userName, String password)  {
+        Boolean value=false;
+        String line = "";
+        String splitBy = ",";
+        try
+        {
+
+            BufferedReader br = new BufferedReader(new FileReader("/home/narendran/IdeaProjects/csci-5408-w2021-project-group15/src/com/package/presentation/users.csv"));
+            while ((line = br.readLine()) != null)
+            {
+                String[] users = line.split(splitBy);
+                if(users[0].equals(userName) && users[1].equals(password)){
+                    return true;
+                }
+            }
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+}
