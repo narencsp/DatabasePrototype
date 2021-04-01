caf02e2f50201e52c5286f4bbd1c15550a8f8b3d
Philemon Lee <philemon.lee@dal.ca>
2021-04-01 12:59:12 -0300

    Added constructor to Table class. Updated tests.

--- src/com/package/data/Table.java
+++ src/com/package/data/Table.java
@@ -2,11 +2,20 @@ package data;
 
 import java.util.Collection;
 
-public abstract class Table {
-    private Collection<Column> rows;
-    public Collection<Column> getRows() {
+public class Table {
+    private Collection<Row> rows;
+
+    public Table(Collection<Row> rows) {
+        this.rows = rows;
+    }
+
+    public Collection<Row> getRows() {
         return rows;
     }
-    abstract void addRow();
-    abstract void removeRow();
+    public void addRow(Row row) {
+        rows.add(row);
+    }
+    public void removeRow(Row row) {
+        rows.remove(row);
+    }
 }
--- src/tests/data/DatabaseTest.java
+++ src/tests/data/DatabaseTest.java
@@ -3,12 +3,26 @@ package data;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class DatabaseTest {
 
+    Database testDatabase = null;
+    Table testTable = null;
+
     @Before
     public void setUp() throws Exception {
+        testDatabase = new Database(new LinkedList<>());
+        testTable = new Table(new LinkedList<>());
+        for (int i = 0; i < 10; i++) {
+            List<Value> valueList = new LinkedList<>();
+            for (int j = 0; j < 3; j++) {
+                valueList.add(new Value("row" + j + "value" + j));
+            }
+            testTable.addRow(new Row(valueList));
+        }
+        testDatabase.addTable(testTable);
     }
 
     @Test
@@ -16,10 +30,14 @@ public class DatabaseTest {
         String test = "Test";
         Object o = test;
         System.out.println(o.toString());
+        Database d = new Database(new LinkedList<>());
+
+        Value testValue = new Value("Hello World!");
     }
 
     @Test
     public void createTable() {
+
     }
 
     @Test
