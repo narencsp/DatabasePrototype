e4d4e743fb9c91b120da119a4f6667edcd003c63
Philemon Lee <philemon.lee@dal.ca>
2021-04-02 14:56:52 -0300

    Fixed var in Database and Table.

--- src/com/package/data/Table.java
+++ src/com/package/data/Table.java
@@ -1,21 +1,39 @@
 package data;
 
 import java.util.Collection;
+import java.util.ArrayList;
 
 public class Table {
-    private Collection<Row> rows;
+    private String name;
+    private final Collection<Row> rows;
 
-    public Table(Collection<Row> rows) {
+    public Table(String name, Collection<Row> rows) {
+        this.name = name;
         this.rows = rows;
+    }
+
+    public Table(String name) {
+        this.name = name;
+        this.rows = new ArrayList<>();
     }
 
     public Collection<Row> getRows() {
         return rows;
     }
+
     public void addRow(Row row) {
         rows.add(row);
     }
+
     public void removeRow(Row row) {
         rows.remove(row);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
